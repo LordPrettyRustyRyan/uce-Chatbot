56f6c528c2f1cf550aa72bc34d0a59d6e358b012
Sidharth <183514714+LordPrettyRustyRyan@users.noreply.github.com>
2026-01-21 20:37:55 +0530

    Refactor chat form submission and message handling

--- static/app.js
+++ static/app.js
@@ -31,11 +31,3 @@ function addMessage(text, type) {
   log.scrollTop = log.scrollHeight;
   return div;
 }
-
-  const PAUSED = true;
-  if (PAUSED) {
-    document.body.innerHTML = "<h1>Uce is offline</h1>";
-  }
-
-
-
